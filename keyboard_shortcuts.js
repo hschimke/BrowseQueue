f45328b484c267f80e4da72477afe6a53210088b
Henry Schimke <henry@podhumphrey.tree>
2009-12-22 14:10:41 -0600

    Added keyboard shortcuts, updated to 1.0.1.0

---
+++ keyboard_shortcuts.js
@@ -0,0 +1,16 @@
+// Add a keyboard listener on keyup.
+if (window == top) {
+  window.addEventListener("keyup", keyListener, false);
+}
+ 
+// Keyboard keyup listener callback.
+function keyListener(e) {
+  // Must press ctrl key to validate.
+  if (e.ctrlKey && e.keyCode && !e.metaKey && e.keyCode != 16 && e.keyCode != 17 && e.keyCode != 18) {
+	chrome.extension.sendRequest({
+      code: e.keyCode,
+      alt: e.altKey,
+      shift: e.shiftKey
+    });
+  }
+}
