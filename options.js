f57173742aec10832176f86a52e6558ce217cf73
Henry A Schimke <henry.schimke+browsequeue@gmail.com>
2010-10-24 16:18:49 -0500

    Integrated jQuery and refactored scripts

--- options.js
+++ options.js
@@ -1,20 +1,25 @@
-// Saves options to localStorage.
+//  Copyright 2010 Henry A Schimke
+//  See license.txt for details
+
+/**
+ * Saves options to localStorage.
+ */
 function save_options_select() {
-  var folder_select = document.getElementById( "folder_list" );
-  var quick_click_select = document.getElementById( "use_quick_click_mode_cb" );
-  
-  var id_hold = folder_select.options[ folder_select.selectedIndex ].value;
-  var quick_click_mode = quick_click_select.checked;
-  
-  localStorage["queue_folder_id"] = id_hold;
-  localStorage["use_quick_click_mode"] = quick_click_mode;
-  
-  chrome.extension.getBackgroundPage().title_folder_id = id_hold;
-  chrome.extension.getBackgroundPage().set_folder_parent();
-  
-  populate_select();
+    var id_hold = $('#folder-list option:selected').val();
+    var quick_click_mode = $('#use-quick-click-mode-cb').attr('checked');
+
+    localStorage["queue_folder_id"] = id_hold;
+    localStorage["use_quick_click_mode"] = quick_click_mode;
+
+    chrome.extension.getBackgroundPage().title_folder_id = id_hold;
+    chrome.extension.getBackgroundPage().set_folder_parent();
+
+    populate_select();
 }
 
+/**
+ * Recurse over bookmarks and add folders to the dropdown list
+ */
 function traverse_bookmarks( results ){
 	for (var j = 0; j < results.length; j++) {
 		var res = results[j];
@@ -29,6 +34,9 @@ function traverse_bookmarks( results ){
 	}
 }
 
+/**
+ * If a bookmark is a folder (has no url) add it to the dropdown list
+ */
 function add_option_bkmrk( mark ){
 	if( mark.id != "0" ){
 		if( mark.url == undefined ){
@@ -37,34 +45,36 @@ function add_option_bkmrk( mark ){
 	}
 }
 
-function add_option( opt_text, op_val ){
-	var folder_select = document.getElementById( "folder_list" );
-				
-	var opt = document.createElement( "option" );
-	opt.text = opt_text;
-	opt.value = op_val;
-	folder_select.options.add( opt );
+/**
+ * Add an option to the folder-list dropdown box
+ * @param {String} opt_text Text for the new item in the dropdown list
+ * @param {String} opt_val Select value for the new item in the dropdown list
+ */
+function add_option( opt_text, opt_val ) {
+    $('<option></option>', {
+        'value': opt_val,
+        'text': opt_text
+    }).appendTo( $('#folder-list') );
 }
 
+/**
+ * Clear and then fill the folder-list dropdown list with bookmarks
+ */
 function populate_select() {
-	document.getElementById( "folder_list" ).options.length = 0;
+	$('#folder-list').html("");
 	chrome.bookmarks.getTree( traverse_bookmarks );
 }
 
+/**
+ * Set the source folder for queued links in the dropdown list
+ */
 function restore_options_2( bkmrk ){
-	var folder_select = document.getElementById( "folder_list" );
-	
-	var total = folder_select.options.length;
-
-	for (var i = 0; i < total; i++) {
-		var child = folder_select.options[i];
-		if (child.value == bkmrk[0].id) {
-			child.selected = "true";
-			break;
-		}
-	}
+    $('#folder-list').val( bkmrk[0].id );
 }
 
+/**
+ * If a folder has been saved, load the select optoins and set the selected folder
+ */
 function restore_options() {
 	var saved_id = localStorage["queue_folder_id"];
 	if (!saved_id) {
@@ -74,11 +84,21 @@ function restore_options() {
 	chrome.bookmarks.get( saved_id, restore_options_2 );
 }
 
+/**
+ * Initialize the page
+ */
 function start_up(){
 	populate_select();
-	if( localStorage["use_quick_click_mode"] == true || localStorage["use_quick_click_mode"] == 'true' ){
-	    var quick_click_select = document.getElementById( "use_quick_click_mode_cb" );
-	    quick_click_select.checked = true;
+	if( localStorage["use_quick_click_mode"] == true || localStorage["use_quick_click_mode"] == 'true' ) {
+	    $('#use-quick-click-mode-cb').attr("checked", true);
+//	    quick_click_select.checked = true;
 	}
 }
 
+$(function() {
+    start_up();
+    $('#save-button').click(
+        function() {
+            save_options_select();
+        } );
+});
--- popup.js
+++ popup.js
@@ -1,16 +1,29 @@
-var enable_disable_function = function() {
-		chrome.extension.getBackgroundPage().en_dis_func();
-		window.close();
-	};
-var queue_pop_function = function() { 
-		chrome.extension.getBackgroundPage().getNextBookmark();
-		window.close();
-	};
+//  Copyright 2010 Henry A Schimke
+//  See license.txt for details
 
+/**
+ * Handle enable/disable button click by calling to background page function
+ */
+function enable_disable_function() {
+	chrome.extension.getBackgroundPage().en_dis_func();
+	window.close();
+}
+
+/**
+ * Handle next button click by calling the background page function
+ */
+function queue_pop_function() { 
+	chrome.extension.getBackgroundPage().getNextBookmark();
+	window.close();
+}
+
+/**
+ * Initialize the popup
+ */
 function startup(){
 	var en_n = chrome.extension.getBackgroundPage().enable_next_button;
 	if( !en_n ){
-		document.getElementById( "pn" ).disabled = true;
+	    $('#next-button').attr( 'disabled', true );
 	}
 	
 	if( localStorage["use_quick_click_mode"] == true || localStorage["use_quick_click_mode"] == 'true' ){
@@ -23,3 +36,15 @@ function startup(){
 	    // #Martin_Weik -- End
     }
 }
+
+$(function() {
+    startup();
+    $('#enable-disable-button').click(
+        function() {
+            enable_disable_function();
+        } );
+    $('#next-button').click( 
+        function() {
+            queue_pop_function();
+        } );
+});
