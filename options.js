574c43867d2b202f6b6ec633966c3ef1731bad8a
Henry Schimke <henry@podhumphrey>
2011-04-04 14:26:40 -0500

    Save, Restore, Validate options page for custom keybindings

--- options.js
+++ options.js
@@ -17,7 +17,7 @@ function save_options_select() {
     var kb_a_key = $('#kb-binding-a-key').val();
     
     var kb_b_mod_1 = $('#kb-binding-b-mod-1 option:selected').val();
-    var kb_b_mod_1 = $('#kb-binding-b-mod-2 option:selected').val();
+    var kb_b_mod_2 = $('#kb-binding-b-mod-2 option:selected').val();
     var kb_b_key = $('#kb-binding-b-key').val();
     
     localStorage["kb_a_mod_1"] = kb_a_mod_1;
@@ -25,7 +25,7 @@ function save_options_select() {
     localStorage["kb_a_key"] = kb_a_key;
     
     localStorage["kb_b_mod_1"] = kb_b_mod_1;
-    localStorage["kb_b_mod_2"] = kb_b_mod_1;
+    localStorage["kb_b_mod_2"] = kb_b_mod_2;
     localStorage["kb_b_key"] = kb_b_key;
 
     // Update bg script settings
@@ -34,6 +34,8 @@ function save_options_select() {
     
     // Reset folder list drop down
     populate_select();
+    // Reset custom keybindings display
+    fill_keybindings();
 }
 
 /**
@@ -104,10 +106,82 @@ function restore_options() {
 }
 
 /**
+ * Fill saved custom keybindings, or load the defaults.
+ */
+function fill_keybindings() {
+    var kb_a_mod_1 = localStorage.getItem("kb_a_mod_1");
+    var kb_a_mod_2 = localStorage.getItem("kb_a_mod_2");
+    var kb_a_key = localStorage.getItem("kb_a_key");
+    
+    var kb_b_mod_1 = localStorage.getItem("kb_b_mod_1");
+    var kb_b_mod_2 = localStorage.getItem("kb_b_mod_2");
+    var kb_b_key = localStorage.getItem("kb_b_key");
+    
+    if( kb_a_mod_1 === null ){
+        kb_a_mod_1 = "ctrl";
+    }
+    if( kb_a_mod_2 === null ){
+        kb_a_mod_2 = "shift";
+    }
+    if( kb_a_key === null ){
+        kb_a_key = "E";
+    }
+    
+    if( kb_b_mod_1 === null ){
+        kb_b_mod_1 = "ctrl";
+    }
+    if( kb_b_mod_2 === null ){
+        kb_b_mod_2 = "shift";
+    }
+    if( kb_b_key === null ){
+        kb_b_key = "P";
+    }
+    
+    // Clean and validate A
+    if( kb_a_mod_1 == "none" && kb_a_mod_2 == "none" ){
+        kb_a_mod_1 = "ctrl";
+        kb_a_mod_2 = "shift";
+    }
+    if( kb_a_mod_1 == kb_a_mod_2 ){
+        kb_a_mod_2 = "none";
+    }
+    if( kb_a_key == "" || kb_a_key == " " ){
+        kb_a_key = "E";
+    }
+    
+    // Clean and validate B
+    if( kb_b_mod_1 == "none" && kb_b_mod_2 == "none" ){
+        kb_b_mod_1 = "ctrl";
+        kb_b_mod_2 = "shift";
+    }
+    if( kb_b_mod_1 == kb_b_mod_2 ){
+        kb_b_mod_2 = "none";
+    }
+    if( kb_b_key == "" || kb_b_key == " " ){
+        kb_b_key = "P";
+    }
+    
+    // Verify not identical
+    if( (kb_a_mod_1 == kb_b_mod_1) && (kb_a_mod_2 == kb_b_mod_2) && (kb_a_key == kb_b_key) ){
+        kb_a_key = "E";
+        kb_b_key = "P";
+    }
+    
+    $('#kb-binding-a-mod-1').val(kb_a_mod_1);
+    $('#kb-binding-a-mod-2').val(kb_a_mod_2);
+    $('#kb-binding-a-key').val(kb_a_key);
+
+    $('#kb-binding-b-mod-1').val(kb_b_mod_1);
+    $('#kb-binding-b-mod-2').val(kb_b_mod_2);
+    $('#kb-binding-b-key').val(kb_b_key);
+}
+
+/**
  * Initialize the page
  */
 function start_up(){
 	populate_select();
+    fill_keybindings();
 	if( localStorage["use_quick_click_mode"] == true || localStorage["use_quick_click_mode"] == 'true' ) {
 	    $('#use-quick-click-mode-cb').attr("checked", true);
 //	    quick_click_select.checked = true;
